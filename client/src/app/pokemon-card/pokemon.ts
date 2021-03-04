8c0e5c23cb3df89b2f368a0558ba39ca608c3056
Cassie Schultz <schu4276@morris.umn.edu>
2021-03-03 19:40:18 -0600

    Add type classes
    I am not sure if this is the best way to do this, but this is what I have for now.  I created some custom types in-order or better organize the types of the Pokemon.

--- client/src/app/pokemon-card/pokemon.ts
+++ client/src/app/pokemon-card/pokemon.ts
@@ -1,7 +1,19 @@
+import { stringify } from "@angular/compiler/src/util";
 import { Url } from "url";
 
 export interface Pokemon {
   name: string;
   sprites: Url;
+  types: Types[];
+}
+
+class Types{
+  type: Type;
+  name: string;
+
+}
+
+class Type{
+  name: string;
 }
 
