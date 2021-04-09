32fc0cdb9f5e2bb2adf1df0cd674d7066e188d77
Cassie <schu4276@morris.umn.edu>
2021-04-09 11:33:51 -0500

    Add search filtering by name functionality
    This gives the user the ability to search for pokemon by name

--- client/src/app/pokemon-list/pokemon-list.component.ts
+++ client/src/app/pokemon-list/pokemon-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Pokemon } from '../pokemon-card/pokemon';
 import { DataService } from '../services/data.service';
 
@@ -7,22 +8,42 @@ import { DataService } from '../services/data.service';
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
   pokemons: any[] =[];
   pokemon: Pokemon;
+  public pokemonName: string;
+  getPokemonSub: Subscription;
+  public filteredPokemons: any[];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.getPokemon();
+
+  }
+
+  ngOnDestroy(): void {
+    this.unsub();
+  }
+  public updateFilter(){
+    this.filteredPokemons = this.dataService.filterPokemon( this.pokemons, {name: this.pokemonName});
+  }
+  getPokemon(){
     this.dataService.getPokemon()
     .subscribe((response: any) => {
       response.results.forEach(result=>{
         this.dataService.getPokemonDetails(result.name)
         .subscribe((detailResponse: any) =>
         this.pokemons.push(detailResponse));
+        this.updateFilter();
       });
     });
-    console.log(this.pokemons);
+
+  }
+  unsub(): void {
+    if (this.getPokemonSub) {
+      this.getPokemonSub.unsubscribe();
+    }
   }
 
 }
--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -9,12 +9,13 @@ import { Observable } from 'rxjs';
 })
 export class DataService {
 
+
   pokeUrl = 'https://pokeapi.co/api/v2/pokemon';
 
   constructor(private http: HttpClient) { }
 
   getPokemon(){
-    return this.http.get(this.pokeUrl+`?limit=300`);
+    return this.http.get(this.pokeUrl+`?limit=15`);
   }
 
   getPokemonDetails(name: string){
@@ -23,6 +24,15 @@ export class DataService {
 
   getPokemonByName(name: string): Observable<Pokemon>{
     return this.http.get<Pokemon>(this.pokeUrl+'/'+name);
+  }
+  filterPokemon(pokemons: any[], filters: { name?: string }): any[] {
+    let filteredPokemons = pokemons;
+
+    if(filters.name){
+      filters.name = filters.name.toLowerCase();
+      filteredPokemons = filteredPokemons.filter(pokemon => pokemon.name.toLowerCase().indexOf(filters.name)!== -1);
+    }
+    return filteredPokemons;
   }
 
 }
